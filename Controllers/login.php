283318f006f8886e9ae03fa17c0edc98e1ba7478
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-27 22:30:52 +0300

    edit login

--- Controllers/login.php
+++ Controllers/login.php
@@ -5,26 +5,33 @@ require_once './db_class.php';
 
 try {
     $database = Database::getInstance();
+
+    if(empty($_POST['email']) || empty($_POST['password'])) {
+        header("Location: ../Views/login_form.php?error=empty");
+        exit();
+    }
+
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $user = $database->findOneUser($email, $password);
-    var_dump($user[0]['role']);
 
     if ($user[0]) {
         session_start();
 
         if ($user[0]['role'] == 'admin') {
-            
             header('Location: ../Views/admin_home.php');
             exit(); 
         } elseif ($user[0]['role'] == 'user') {
             header('Location: ../Views/home.php');
             exit(); 
+        } else {
+            header("Location: ../Views/login_form.php?error=invalid_credentials");
+            exit();
+        }
     } else {
-        header("Location: ../Views/login_form.php");
+        header("Location: ../Views/login_form.php?error=invalid_credentials");
         exit();
-     }
     }
 } catch(PDOException $e) {
     echo $e->getMessage(); 
