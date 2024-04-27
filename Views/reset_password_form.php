e587b8b32acebd357b4b16ab85250fec7c067024
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-27 23:47:05 +0300

    modify reset password structure

--- Views/reset_password_form.php
+++ Views/reset_password_form.php
@@ -1,34 +1,3 @@
- <?php
-require_once  '../db_info.php';
-
-$message = '';
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    if ($_POST['new_password'] == $_POST['confirm_password']) {
-        try {
-
-            $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT, DB_USER, DB_PASSWORD);
-
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            $stmt = $pdo->prepare("UPDATE users SET password = :new_password WHERE username = :username");
-            
-            $stmt->bindParam(':new_password', $_POST['new_password']);
-            $stmt->bindParam(':username', $_POST['username']);
-            
-            $stmt->execute();
-            
-            $message = "Password updated successfully!";
-        } catch(PDOException $e) {
-            $message = "Error updating password: " . $e->getMessage();
-        }
-    } else {
-        $message = "Passwords do not match!";
-    }
-}
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
