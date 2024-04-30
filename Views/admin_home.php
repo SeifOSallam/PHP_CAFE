82ca15fbb94650517d6728bffcbfd46d5357045b
Mona Eid <104376815+Monaeid2001@users.noreply.github.com>
2024-04-30 19:24:57 +0300

    fix styling issue

--- Views/admin_home.php
+++ Views/admin_home.php
@@ -18,7 +18,6 @@ $offset = ($page - 1) * $records_per_page;
 <html>
 <head>
     <title>All Users</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <style>
         .add-user-button {
             background-color: #007bff; 
