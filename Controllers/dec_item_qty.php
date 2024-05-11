51932481d6af0459c3e289d3fbc74ff0fe77ae10
omarkhalil117 <omarkhalil117@gmail.com>
2024-05-11 16:57:36 +0300

    handle cart item quantity change

--- Controllers/dec_item_qty.php
+++ Controllers/dec_item_qty.php
@@ -6,7 +6,18 @@ require_once 'db_class.php';
 $update = "quantity=".(int)$_GET['qty']-1;
 
 $res = $database->updateQty('cart',$_GET['product_id'],$update);
+$role = $_GET['role'];
 
-header("Location:../Views/home.php");
+if ($role === 'user')
+{
+    header("Location:../Views/home.php");
+    exit();
+}
+
+if ($role === 'admin')
+{
+    header("Location:../Views/admin_landing_page.php");
+    exit();
+}
 
 ?>
--- Controllers/inc_item_qty.php
+++ Controllers/inc_item_qty.php
@@ -3,7 +3,25 @@
 require_once 'db_class.php';
 
 $update = "quantity=".(int)$_GET['qty']+1;
+$qty = (int)$_GET['qty']+1;
 $role = $_GET['role'];
+
+$product = $database->getProductById($_GET['product_id']);
+
+if($qty > (int)$product[0]['stock'])
+{
+    if ($role === 'admin')
+    {
+        header("Location:../Views/admin_landing_page.php?error=Can not add more from this product");
+        exit();
+    }
+
+    if ($role === 'user')
+    {
+        header("Location:../Views/home.php?error=Can not add more from this product");
+        exit();
+    }
+}
 
 $res = $database->updateQty('cart',$_GET['product_id'],$update);
 
@@ -11,9 +29,11 @@ $res = $database->updateQty('cart',$_GET['product_id'],$update);
 if($role === 'user')
 {
     header("Location:../Views/home.php");
+    exit();
 }
-else
+if($role === 'admin')
 {
     header("Location:../Views/admin_landing_page.php");
+    exit();
 }
 ?>
