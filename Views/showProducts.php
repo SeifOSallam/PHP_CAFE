74497aca48156e6a4c11d3844bb3ef9d157fcac5
AlaaSherif55 <alaa.sherif.fathi@gmail.com>
2024-05-11 12:52:20 +0200

    add

--- Views/showProducts.php
+++ Views/showProducts.php
@@ -81,11 +81,7 @@ $count = $count[0]['count'];
     </div>
   </div>
 
-<script>
-    document.getElementById("closeErrorAlert").addEventListener("click", function() {
-        document.getElementById("errorAlert").remove();
-    });
-</script>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
