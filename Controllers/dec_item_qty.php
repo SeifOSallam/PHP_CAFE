cffd11497fea21a5101bdc5478041f4ce28fc8af
omarkhalil117 <omarkhalil117@gmail.com>
2024-05-11 17:46:12 +0300

    handle zero items in cart

--- Controllers/dec_item_qty.php
+++ Controllers/dec_item_qty.php
@@ -5,8 +5,29 @@ require_once 'db_class.php';
 
 $update = "quantity=".(int)$_GET['qty']-1;
 
-$res = $database->updateQty('cart',$_GET['product_id'],$update);
 $role = $_GET['role'];
+$qty = (int)$_GET['qty']-1;
+$product_id = $_GET['product_id'];
+$user_id = $_SESSION['id'];
+
+if ($qty === 0)
+{
+    $database->deleteItem('cart',$product_id,$user_id);
+
+    if ($role === 'admin')
+    {
+        header("Location:../Views/admin_landing_page.php?error=can not be zero");
+        exit();
+    }
+    
+    if ($role === 'user')
+    {
+        header("Location:../Views/home.php?error=can not be zero");
+        exit();
+    }
+}
+
+$res = $database->updateQty('cart',$_GET['product_id'],$update);
 
 if ($role === 'user')
 {
